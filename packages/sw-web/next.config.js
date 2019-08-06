2fae824dd1f2f406418a7b80a7708b73dedc1540
Tuan Nguyen <tuan.nguyen@hiivesystems.com>
2019-08-07 00:12:19 +1000

    feat($build): show nextjs bundle size

--- packages/sw-web/next.config.js
+++ packages/sw-web/next.config.js
@@ -1,28 +1,43 @@
 const path = require('path');
 const paths = require('sportywide-shared/build/paths');
 const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
-module.exports = withCustomBabelConfigFile({
-	babelConfigFile: path.resolve(paths.shared.webpack, 'react', 'babel', 'babel.config.js'),
-	webpack: config => {
-		const oldConfig = config;
-		config.resolve = {
-			...(oldConfig.resolve || {}),
-			alias: {
-				...oldConfig.resolve.alias,
-				'@shared': 'sportywide-shared/src',
-				'@web': paths.web.src + '/',
+module.exports = withBundleAnalyzer(
+	withCustomBabelConfigFile({
+		babelConfigFile: path.resolve(paths.shared.webpack, 'react', 'babel', 'babel.config.js'),
+		webpack: config => {
+			const oldConfig = config;
+			config.resolve = {
+				...(oldConfig.resolve || {}),
+				alias: {
+					...oldConfig.resolve.alias,
+					'@shared': 'sportywide-shared/src',
+					'@web': paths.web.src + '/',
+				},
+			};
+			return config;
+		},
+		analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+		analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+		bundleAnalyzerConfig: {
+			server: {
+				analyzerMode: 'static',
+				reportFilename: './bundle-analyze/server.html',
 			},
-		};
-		return config;
-	},
-	webpackDevMiddleware: config => {
-		config.watchOptions = {
-			poll: 1000, // Check for changes every second
-			aggregateTimeout: 300, // delay before rebuilding
-		};
-		return config;
-	},
-	dest: 'dist',
-	dir: 'src',
-});
+			browser: {
+				analyzerMode: 'static',
+				reportFilename: './bundle-analyze/client.html',
+			},
+		},
+		webpackDevMiddleware: config => {
+			config.watchOptions = {
+				poll: 1000, // Check for changes every second
+				aggregateTimeout: 300, // delay before rebuilding
+			};
+			return config;
+		},
+		dest: 'dist',
+		dir: 'src',
+	})
+);
