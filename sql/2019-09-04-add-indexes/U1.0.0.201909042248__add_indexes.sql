32abfa34c3a43ccebfbab7d7e8a7dd73b54943aa
Tuan Nguyen <tuan.nguyen@hiivesystems.com>
2019-09-04 23:11:50 +1000

    impr(database): restructure database table

---
+++ sql/2019-09-04-add-indexes/U1.0.0.201909042248__add_indexes.sql
@@ -0,0 +1,2 @@
+DROP INDEX idx_token_engagement;
+DROP INDEX idx_token_content;
---
+++ sql/2019-09-04-add-indexes/V1.0.0.201909042248__add_indexes.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_token_engagement ON tokens (engagement_table, engagement_id);
+CREATE INDEX idx_token_content ON tokens (content);
