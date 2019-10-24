56b6eae42bb0ea0ef66a00d4c2044036656b70f5
Tuan Nguyen <vdtn359@gmail.com>
2019-10-25 09:12:39 +1100

    feat(sw-api): generate 15 random players. added ability to run tests in docker

--- packages/sw-data/src/persister/persister.module.ts
+++ packages/sw-data/src/persister/persister.module.ts
@@ -1,17 +1,21 @@
+import { SchemaPlayerModule } from '@schema/player/player.module';
 import { Module } from '@nestjs/common';
 import { PlayerPersisterService } from '@data/persister/player/player-persister.service';
 import { CoreSchemaModule } from '@schema/core/core-schema.module';
 import { SqlConnectionModule } from '@schema/core/connection/sql-connection.module';
 import { DATA_CONFIG, SCHEMA_CONFIG } from '@core/config/config.constants';
-import { SchemaUserModule } from '@schema/user/user.module';
 import { CoreDataModule } from '@data/core/core-data.module';
 import { TeamPersisterService } from '@data/persister/team/team-persister.service';
 import { FixturePersisterService } from '@data/persister/fixture/fixture-persister.service';
+import { SchemaTeamModule } from './../../../sw-schema/src/team/team.module';
+import { SchemaFixtureModule } from './../../../sw-schema/src/fixture/fixture.module';
 
 @Module({
 	imports: [
-		SchemaUserModule,
 		CoreDataModule,
+		SchemaPlayerModule,
+		SchemaTeamModule,
+		SchemaFixtureModule,
 		SqlConnectionModule.forRootAsync({
 			inject: [SCHEMA_CONFIG, DATA_CONFIG],
 			useFactory: (schemaConfig, dataConfig) => ({
