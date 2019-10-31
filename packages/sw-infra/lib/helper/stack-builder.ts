993b71a56652a07b776d617493e1cc9b262d68b1
Tuan Nguyen <vdtn359@gmail.com>
2019-11-01 08:47:51 +1100

    feat(sw-infra): implement NAT gateway

--- packages/sw-infra/lib/helper/stack-builder.ts
+++ packages/sw-infra/lib/helper/stack-builder.ts
@@ -1,7 +1,18 @@
-import { Construct, Resource, Stack } from '@aws-cdk/core';
-import { SubnetConfiguration, Vpc, VpcProps, SecurityGroup, SecurityGroupProps, Port, IPeer } from '@aws-cdk/aws-ec2';
+import { CfnCondition, CfnConditionProps, Construct, Resource, Stack } from '@aws-cdk/core';
+import {
+	CfnInstance,
+	CfnInstanceProps,
+	IPeer,
+	Port,
+	SecurityGroup,
+	SecurityGroupProps,
+	SubnetConfiguration,
+	Vpc,
+	VpcProps,
+} from '@aws-cdk/aws-ec2';
 import { ucfirst } from '@shared/lib/utils/string/conversion';
-import { DatabaseInstanceProps, DatabaseInstance } from '@aws-cdk/aws-rds';
+import { DatabaseInstance, DatabaseInstanceProps } from '@aws-cdk/aws-rds';
+import { CfnCacheCluster, CfnCacheClusterProps, CfnSubnetGroup, CfnSubnetGroupProps } from '@aws-cdk/aws-elasticache';
 
 type SwSecurityGroupProps = SecurityGroupProps & {
 	ingressRules?: {
@@ -14,32 +25,75 @@ type SwSecurityGroupProps = SecurityGroupProps & {
 	}[];
 };
 export class StackBuilder {
-	private readonly stack: Stack;
+	readonly stack: Stack;
 	private readonly resourceMap: Map<string, Construct>;
+	readonly app: Construct;
 
 	constructor(scope: Construct, id: string, props?: any) {
-		this.stack = new Stack(scope, id, props);
+		this.app = scope;
+		this.stack = new Stack(scope, this.getName(id), props);
 		this.resourceMap = new Map<string, Resource>();
-		this.resourceMap.set(id, this.stack);
+		this.resourceMap.set(this.getName(id), this.stack);
+	}
+
+	cfnCondition(name, props: CfnConditionProps) {
+		return this.register(name, new CfnCondition(this.stack, this.getName(name), props));
+	}
+
+	register(name, instance) {
+		this.resourceMap.set(this.getName(name), instance);
+		return instance;
 	}
 
 	vpc(name, props: VpcProps): Vpc {
-		return new Vpc(this.stack, this.getName(name), props);
+		return this.register(name, new Vpc(this.stack, this.getName(name), props));
 	}
 
 	securityGroup(name, { ingressRules = [], egressRules = [], ...props }: SwSecurityGroupProps): SecurityGroup {
-		const securityGroup = new SecurityGroup(this.stack, this.getName(name), props);
+		const securityGroup = new SecurityGroup(this.stack, this.getName(name), {
+			securityGroupName: this.getName(name),
+			...props,
+		});
 		for (const rule of ingressRules) {
 			securityGroup.addIngressRule(rule.peer, rule.port);
 		}
 		for (const rule of egressRules) {
 			securityGroup.addEgressRule(rule.peer, rule.port);
 		}
-		return securityGroup;
+		return this.register(name, securityGroup);
 	}
 
-	rds(name, props: DatabaseInstanceProps) {
-		return new DatabaseInstance(this.stack, this.getName(name), props);
+	ec2(name, props: CfnInstanceProps) {
+		return this.register(
+			name,
+			new CfnInstance(this.stack, this.getName(name), {
+				...props,
+			})
+		);
+	}
+
+	rds(name, props: DatabaseInstanceProps): DatabaseInstance {
+		return this.register(name, new DatabaseInstance(this.stack, this.getName(name), props));
+	}
+
+	cacheSubnetGroup(name, props: CfnSubnetGroupProps) {
+		return this.register(
+			name,
+			new CfnSubnetGroup(this.stack, this.getName(name), {
+				cacheSubnetGroupName: this.getName(name),
+				...props,
+			})
+		);
+	}
+
+	cacheCluster(name, props: CfnCacheClusterProps) {
+		return this.register(
+			name,
+			new CfnCacheCluster(this.stack, this.getName(name), {
+				clusterName: this.getName(name),
+				...props,
+			})
+		);
 	}
 
 	subnet(props: SubnetConfiguration): SubnetConfiguration {
@@ -49,10 +103,6 @@ export class StackBuilder {
 			cidrMask: props.cidrMask,
 			reserved: props.reserved,
 		};
-	}
-
-	build() {
-		return this.stack;
 	}
 
 	get(name) {
