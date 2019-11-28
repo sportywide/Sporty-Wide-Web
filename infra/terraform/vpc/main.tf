c9f6c2abed3d4d7f3af61a3269a87eb1e2239775
Tuan Nguyen <vdtn359@uowmail.edu.au>
2019-11-28 16:13:36 +1100

    feat(infra): added scripts module for terraform

--- infra/terraform/vpc/main.tf
+++ infra/terraform/vpc/main.tf
@@ -6,89 +6,6 @@ resource "aws_vpc" "vpc" {
   tags = merge(var.tags, { "Name" = "sw-vpc" })
 }
 
-resource "aws_key_pair" "ec2_key_pair" {
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6fZ1R1aGn4gYQCQlSSM0BlQQHEeqHkozK3qvVAOCtr64wJ7L0e6PZQ26PPx8tk/SJafpM+FfFbaNfdcNLIBmkDWjYFeHFJGMHi6RvYwe4Is4807Nrixf9v2FBiKQ4XImhL5TXLAeXKKh47Yq/xS1nb/VMFAfPDzWVvWK9TiLnCnza5pDr10HO2pPrCbjlnGu/u1v3tG/YsuxtyAh9PXbDAwUlfZqbvvJB5CAnk/ptJj7q8Le24svbeCj0ZyHWIiyfmciUg27oskcBYleoLMIhSGR5YEqwOIKTW70w2YnVTNiaB9VgtusUmglUkgklKsc1NmgPQULRldAH7eUxolJTVAON2GT1b8s9X5Ljcuh2NVfiV6//XaXK+FnPxoGY5S/jOZzan+ilwtRCpyb3tSQFwBa8mTOygQocyrojdynmKzd9GemTzYHi5C5Y0RPV/Eb+ZTpotdbeGP0YdGcIbciEvEhLFI6m/cXQprcUCw21FSC2/nV2Qw8fpvUPjfIYYQABcxKZFyAXLg+ay/ECt7OnTt9YHPaVLvWm1TUPlwvRyQeGlTCPyS7sSis5ReB35jUbAynVMdoBu2FY3eWbS888QmOpIUjM+2A03zgowgOcEQ9froezQSCrW/8ceNBA6VWkGP2R/eFXpjdHoTUPyPtl1/IMxINdk9zuQpVmKSdeFw== tuannguyen@NASQL10657"
-  key_name = "sw-ec2-key"
-}
-
-resource "aws_eip" "nat_eip" {
-  tags = var.tags
-}
-
-resource "aws_security_group" "nat_security_group" {
-  ingress {
-    from_port = 22
-    protocol = "TCP"
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-  vpc_id = aws_vpc.vpc.id
-  tags = var.tags
-  name = "sw-nat-security-group"
-}
-
-resource "aws_instance" "nat_instance" {
-  ami = "ami-00c1445796bc0a29f"
-  instance_type = "t2.nano"
-  depends_on = [aws_key_pair.ec2_key_pair]
-  tags = merge(var.tags, { "Name" = "sw-ec2-nat" })
-  volume_tags = var.tags
-  user_data = <<EOF
-Content-Type: multipart/mixed; boundary="//"
-MIME-Version: 1.0
-
---//
-Content-Type: text/cloud-config; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="cloud-config.txt"
-
-#cloud-config
-cloud_final_modules:
-- [scripts-user, always]
-
---//
-Content-Type: text/x-shellscript; charset="us-ascii"
-MIME-Version: 1.0
-Content-Transfer-Encoding: 7bit
-Content-Disposition: attachment; filename="userdata.txt"
-
-#!/bin/bash
-set -x
-exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
-yum update -y
-yum install -y postgresql
-yum install -y gcc
-yum install -y git
-wget http://download.redis.io/redis-stable.tar.gz && tar xvzf redis-stable.tar.gz && cd redis-stable && make
-cp src/redis-cli /usr/bin/
-yum install -y jq
-yum install -y docker
-service docker start
-usermod -aG docker ec2-user
---//
-EOF
-  subnet_id = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.nat_security_group.id]
-  monitoring = true
-  key_name = aws_key_pair.ec2_key_pair.key_name
-  lifecycle {
-    ignore_changes = [user_data]
-  }
-}
-
-resource "aws_eip_association" "nat_eip_association" {
-  instance_id = aws_instance.nat_instance.id
-  allocation_id = aws_eip.nat_eip.id
-}
-
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "172.16.0.0/24"
@@ -139,7 +56,7 @@ resource "aws_route_table" "private_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    instance_id = aws_instance.nat_instance.id
+    instance_id = module.ec2.nat_instance_id
   }
 
   tags = var.tags
@@ -162,7 +79,7 @@ module "rds" {
   db_username = var.db_username
   private_subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   vpc_id = aws_vpc.vpc.id
-  nat_security_group_id = aws_security_group.nat_security_group.id
+  nat_security_group_id = module.ec2.nat_security_group_id
 }
 
 module "redis" {
@@ -170,5 +87,25 @@ module "redis" {
   tags = var.tags
   private_subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   vpc_id = aws_vpc.vpc.id
-  nat_security_group_id = aws_security_group.nat_security_group.id
+  nat_security_group_id = module.ec2.nat_security_group_id
+}
+
+module "ec2" {
+  source = "./ec2"
+  public_subnet_id = aws_subnet.public_subnet.id
+  tags = var.tags
+  vpc_id = aws_vpc.vpc.id
+}
+
+module "ssm" {
+  source = "./ssm"
+  db_password = var.db_password
+  db_username = var.db_username
+  db_endpoint = module.rds.rds_dns
+}
+
+module "scripts" {
+  source = "./scripts"
+  nat_dns = module.ec2.nat_instance_dns
+  rds_dns = module.rds.rds_dns
 }
