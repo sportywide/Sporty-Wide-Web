d9983e98d3af3ed2f2d9635f499f981f227cbcd3
Tuan Nguyen <vdtn359@uowmail.edu.au>
2019-12-01 15:42:27 +1100

    feat(build): create docker file for api and email

--- packages/sw-web/webpack.config.ts
+++ packages/sw-web/webpack.config.ts
@@ -4,7 +4,7 @@ import { makeConfig } from '@build/webpack/node/config';
 import paths from '@build/paths';
 const argv = require('yargs').argv;
 
-module.exports = makeConfig({
+const config = makeConfig({
 	hot: isDevelopment(argv.env),
 	env: argv.env,
 	entries: path.resolve(paths.web.src, 'next-server'),
@@ -15,3 +15,7 @@ module.exports = makeConfig({
 		'@web': paths.web.src,
 	},
 });
+
+(config.optimization = config.optimization || {}).minimize = false;
+
+module.exports = config;
