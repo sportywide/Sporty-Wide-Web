af8d483fc6dbcab016c15108e22243e21447e973
Tuan Nguyen <tuannguyen@nine.com.au>
2019-12-12 14:04:03 +1100

    feat(sw-data): fetch live score

--- packages/sw-scheduling/src/lambda/crawl/fetch-live-score.ts
+++ packages/sw-scheduling/src/lambda/crawl/fetch-live-score.ts
@@ -5,16 +5,18 @@ import { groupBy } from 'lodash';
 import { leagues } from '@shared/lib/data/data.constants';
 import { FixtureService } from '@schema/fixture/services/fixture.service';
 import { TeamService } from '@schema/team/services/team.service';
-import { Fixture } from '@schema/fixture/models/fixture.entity';
+import { DATA_LOGGER } from '@core/logging/logging.constant';
 
-export async function handler() {
+export async function handler(event, context) {
 	let whoscoreCrawler;
 	try {
+		context.callbackWaitsForEmptyEventLoop = false;
 		const module = await initModule(SchedulingModule);
 		whoscoreCrawler = module.get(WhoScoreCrawlerService);
-		const date = new Date('2019-12-08');
+		const date = new Date();
 		const matches = await whoscoreCrawler.getLiveMatches(date);
 		const leagueMatches = groupBy(matches, 'whoscoreLeagueId');
+		const logger = module.get(DATA_LOGGER);
 		const whoscoreLeagueIds = Object.keys(leagueMatches);
 		const fixtureService = module.get(FixtureService);
 		const teamsService = module.get(TeamService);
@@ -25,17 +27,16 @@ export async function handler() {
 				leagueId: league.id,
 				date,
 			});
-			const result: Map<any, Fixture> = new Map();
 			const dbTeams = await teamsService.findByLeague(league.id);
 			for (const match of leagueMatches[league.whoscoreId]) {
 				const homeDbTeam = teamsService.fuzzySearch(dbTeams, match.home);
 				if (!homeDbTeam) {
-					console.error('Cannot find the team', match.home);
+					logger.error('Cannot find the team', match.home);
 					continue;
 				}
 				const awayDbTeam = teamsService.fuzzySearch(dbTeams, match.away);
 				if (!awayDbTeam) {
-					console.error('Cannot find the team', match.away);
+					logger.error('Cannot find the team', match.away);
 					continue;
 				}
 
@@ -43,11 +44,12 @@ export async function handler() {
 					fixture => fixture.awayId === awayDbTeam.id && fixture.homeId === homeDbTeam.id
 				);
 				if (!dbFixture) {
-					console.error(`Cannot find fixture for teams ${match.home} - ${match.away}`);
+					logger.error(`Cannot find fixture for teams ${match.home} - ${match.away}`);
 				}
 				dbFixture.whoscoreUrl = match.link;
 				dbFixture.incidents = match.incidents;
 				await fixtureService.saveOne(dbFixture);
+				logger.debug('Saving fixture', dbFixture.id);
 			}
 		}
 	} catch (e) {
